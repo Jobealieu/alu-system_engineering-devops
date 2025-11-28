602ab728d9b1520e17a097df98dbd16e870a0a23
Jobealieu <a.jobe@alustudent.com>
2025-11-28 23:06:38 +0200

    Fix line length issue in 1-user_limit.pp

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,6 @@
 # This manifest fixes the "Too many open files" error
 
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/" /etc/security/limits.conf && sed -i "s/holberton soft nofile 4/holberton soft nofile 50000/" /etc/security/limits.conf',
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  command => 'bash -c "sed -i \"s/holberton hard nofile 5/holberton hard nofile 50000/\" /etc/security/limits.conf && sed -i \"s/holberton soft nofile 4/holberton soft nofile 50000/\" /etc/security/limits.conf"',
+  path    => ['/bin', '/usr/bin', '/usr/sbin', '/usr/local/bin'],
 }
