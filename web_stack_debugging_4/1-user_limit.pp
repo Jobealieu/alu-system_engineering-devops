366b0bdaa4789378e588437614dcd5d12f3ddab1
Jobealieu <a.jobe@alustudent.com>
2025-11-28 23:22:09 +0200

    Fix line length issue in 1-user_limit.pp

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 # Increase file descriptor limits for holberton user to prevent 'Too many open files' error
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'bash -c "echo \"holberton soft nofile 4096\" >> /etc/security/limits.conf && echo \"holberton hard nofile 8192\" >> /etc/security/limits.conf"',
-  path    => ['/usr/bin', '/bin'],
-  unless  => 'grep -q "holberton.*nofile" /etc/security/limits.conf',
+  command => 'sed -i "/holberton/d" /etc/security/limits.conf; \
+echo "holberton soft nofile 4096" >> /etc/security/limits.conf; \
+echo "holberton hard nofile 8192" >> /etc/security/limits.conf',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
 }
